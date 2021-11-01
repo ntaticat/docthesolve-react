4e816400b16dfa30d99301338406c9cc765167a5
Rafael Estrada <ntaticat@gmail.com>
2021-10-31 22:03:59 -0600

    configure routing in app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
 import './App.css';
 import { Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link, useRouteMatch, useParams } from 'react-router-dom'
+import { AssistantMainPage } from './components/assistant/assistant-main-page/AssistantMainPage'
+import { IncidencesPage } from './components/assistant/incidences-page/IncidencesPage'
+
 
 function App() {
   return (
-    <Fragment>
-      <h1>Hola Mundo</h1>
-    </Fragment>
+    <Router>
+      <div className="">
+        <h1>DocTheSolve</h1>
+        {/* Menu de navegación */}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Inicio</Link>
+            </li>
+            <li>
+              <Link to="/incidences">Incidencias</Link>
+            </li>
+          </ul>
+        </nav>
+        
+        {/* Routing */}
+        <Switch>
+          <Route exact path="/">
+            <AssistantMainPage />
+          </Route>
+          <Route path="/incidences">
+            <IncidencesPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
