124d7a149739cf1e1a54c1c1f0cfe275d27090e2
Rafael Estrada <ntaticat@gmail.com>
2021-10-31 21:38:10 -0600

    add assistant-main-page component

---
+++ src/components/assistant/assistant-main-page/AssistantMainPage.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const AssistantMainPage = () => {
+  return (
+    <Fragment>
+      <h1>Página Principal del Asistente Técnico</h1>
+    </Fragment>
+  );
+};
+
+export default AssistantMainPage;
