14df8c7d553f73e5c7c93b21d6c2f88210e56dde
Rafael Estrada <ntaticat@gmail.com>
2021-10-31 21:38:39 -0600

    add incidences-page component

---
+++ src/components/assistant/incidences-page/IncidencesPage.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const IncidencesPage = () => {
+  return (
+    <Fragment>
+      <h1>Página de Incidencias del Asistente Técnico</h1>
+    </Fragment>
+  );
+};
+
+export default IncidencesPage;
